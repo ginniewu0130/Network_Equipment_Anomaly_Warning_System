e925a40d8722005d4ef6f341f247e59e15e84b20
ginniewu0130 <chutm08@gmail.com>
2023-07-16 00:50:52 +0800

    修改logchart、新增DisplayLog控制器

---
+++ PJ_Login/Controllers/DisplayLogController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJ_Login.Data;
+using System.Collections.Generic;
+using System.Linq;
+using PJ_Login.Models;
+using PJ_Login.ViewModels;
+
+namespace PJ_Login.Controllers
+{
+    public class DisplayLogController : Controller
+    {
+        private readonly ChartDBContext _ctx;
+        public DisplayLogController(ChartDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IActionResult DisplayLogList()
+        {
+            List<LogChartAnomly> abnomalLog  = _ctx.LogChartAnomlies.ToList();
+            List<AbnomalLogViewmodel> abnomalLogVM = new List<AbnomalLogViewmodel>();
+            foreach(var log in abnomalLog)
+            {
+                abnomalLogVM.Add(new AbnomalLogViewmodel
+                {
+                    SourceIp = log.SourceIp,
+                    DestinationIp = log.DestinationIp,
+                    SourcePort = log.SourcePort,
+                    DistinationPort = log.DistinationPort,
+                    Action = log.Action
+                });
+            }
+
+            return View(abnomalLogVM);
+        }
+    }
+}
---
+++ PJ_Login/ViewModels/AbnomalLogViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using PJ_Login.Models;
+using System;
+
+namespace PJ_Login.ViewModels
+{
+    public class AbnomalLogViewmodel
+    {
+        public double? SourceIp { get; set; }
+        public double? DestinationIp { get; set; }
+        public int? SourcePort { get; set; }
+        public int? DistinationPort { get; set; }
+        public string Action { get; set; }
+    }
+}
